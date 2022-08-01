d42bc7c74718761654ebdb8afae6dca93630e426
Joshua Peisach <itzswirlz2020@outlook.com>
2022-07-31 21:05:00 -0400

    Progess update
    Added songs: Epidox, Obsidian, Minds of the Mad
    
    Completed obsidian texture

--- src/main/java/com/github/ItzSwirlz/CloneHeroMusicDiscs/items/CHMDItems.java
+++ src/main/java/com/github/ItzSwirlz/CloneHeroMusicDiscs/items/CHMDItems.java
@@ -16,8 +16,11 @@ public class CHMDItems {
 
 	public static final RegistryObject<Item> MUSIC_DISC_COALESCENCE_AND_SEGMENTATION = ITEMS.register("music_disc.coalescenceandsegmentation", () -> new RecordItem(16, CHMDSoundEvents.COALESCENCE_AND_SEGMENTATION, (new Item.Properties()).stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE), 25160));
     public static final RegistryObject<Item> MUSIC_DISC_CONSTELLATION = ITEMS.register("music_disc.constellation", () -> new RecordItem(17, CHMDSoundEvents.CONSTELLATION, (new Item.Properties()).stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE), 18280));
-    public static final RegistryObject<Item> MUSIC_DISC_REMINISCE = ITEMS.register("music_disc.reminisce", () -> new RecordItem(18, CHMDSoundEvents.REMINISCE, (new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE)), 2280));
-    public static final RegistryObject<Item> MUSIC_DISC_SOLAR_STORM = ITEMS.register("music_disc.solarstorm", () -> new RecordItem(19, CHMDSoundEvents.SOLAR_STORM, (new Item.Properties()).stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE), 4080));
+    public static final RegistryObject<Item> MUSIC_DISC_EPIDOX = ITEMS.register("music_disc.epidox", () -> new RecordItem(18, CHMDSoundEvents.EPIDOX, (new Item.Properties()).stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE), 6520));
+    public static final RegistryObject<Item> MUSIC_DISC_MINDS_OF_THE_MAD = ITEMS.register("music_disc.mindsofthemad", () -> new RecordItem(19, CHMDSoundEvents.MINDS_OF_THE_MAD, (new Item.Properties()).stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE), 5240));
+    public static final RegistryObject<Item> MUSIC_DISC_OBSIDIAN = ITEMS.register("music_disc.obsidian", () -> new RecordItem(20, CHMDSoundEvents.OBSIDIAN, (new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE)), 4440));
+    public static final RegistryObject<Item> MUSIC_DISC_REMINISCE = ITEMS.register("music_disc.reminisce", () -> new RecordItem(21, CHMDSoundEvents.REMINISCE, (new Item.Properties().stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE)), 2280));
+    public static final RegistryObject<Item> MUSIC_DISC_SOLAR_STORM = ITEMS.register("music_disc.solarstorm", () -> new RecordItem(22, CHMDSoundEvents.SOLAR_STORM, (new Item.Properties()).stacksTo(1).tab(CreativeModeTab.TAB_MISC).rarity(Rarity.RARE), 4080));
 
     public static void registerItems() {
 		ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
--- src/main/java/com/github/ItzSwirlz/CloneHeroMusicDiscs/soundevents/CHMDSoundEvents.java
+++ src/main/java/com/github/ItzSwirlz/CloneHeroMusicDiscs/soundevents/CHMDSoundEvents.java
@@ -13,6 +13,9 @@ public class CHMDSoundEvents {
 
 	public static final RegistryObject<SoundEvent> COALESCENCE_AND_SEGMENTATION = SOUNDEVENTS.register("music_disc.coalescenceandsegmentation", () -> new SoundEvent(new ResourceLocation("cloneheromusicdiscs", "music_disc.coalescenceandsegmentation")));
     public static final RegistryObject<SoundEvent> CONSTELLATION = SOUNDEVENTS.register("music_disc.constellation", () -> new SoundEvent(new ResourceLocation("cloneheromusicdiscs", "music_disc.constellation")));
+    public static final RegistryObject<SoundEvent> EPIDOX = SOUNDEVENTS.register("music_disc.epidox", () -> new SoundEvent(new ResourceLocation("cloneheromusicdiscs", "music_disc.epidox")));
+    public static final RegistryObject<SoundEvent> MINDS_OF_THE_MAD = SOUNDEVENTS.register("music_disc.mindsofthemad", () -> new SoundEvent(new ResourceLocation("cloneheromusicdiscs", "music_disc.mindsofthemad")));
+    public static final RegistryObject<SoundEvent> OBSIDIAN = SOUNDEVENTS.register("music_disc.obsidian", () -> new SoundEvent(new ResourceLocation("cloneheromusicdiscs", "music_disc.obsidian")));
     public static final RegistryObject<SoundEvent> REMINISCE = SOUNDEVENTS.register("music_disc.reminisce", () -> new SoundEvent(new ResourceLocation("cloneheromusicdiscs", "music_disc.reminisce")));
     public static final RegistryObject<SoundEvent> SOLAR_STORM = SOUNDEVENTS.register("music_disc.solarstorm", () -> new SoundEvent(new ResourceLocation("cloneheromusicdiscs", "music_disc.solarstorm")));
 
